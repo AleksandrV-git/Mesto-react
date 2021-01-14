cdf6f55ebfdcba355fb4de95f0b469c22ac0a433
AleksandrV <kolomel@yandex.ru>
2021-01-14 21:55:03 +0300

    fix Card, add isLiked, isOwn

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 function Card(props) {
 
+  const currentUser = React.useContext(CurrentUserContext);
+  const card = props.card; 
+  const isOwn = card.owner._id === currentUser._id;
+  const cardDeleteButtonStyle = isOwn ? "block" : 'none';
+  const isLiked = card.likes.some(i => i._id === currentUser._id);
+  const cardLikeButtonClassName = (
+    `place-card__like-icon ${isLiked ? 'place-card__like-icon_liked' : ''}`
+  ); 
+
   function handleClick() {
-    props.onCardClick(props.card);
+    props.onCardClick(card);
   }   
 
   return (
     <>
       <div className="place-card">
-        <div className="place-card__image" style={{ backgroundImage: `url(${props.card.link})` }} onClick={handleClick} >
-          <button className="place-card__delete-icon"></button>
+        <div className="place-card__image" style={{ backgroundImage: `url(${card.link})` }} onClick={handleClick} >
+          <button className="place-card__delete-icon" style={{ display: `${cardDeleteButtonStyle}` }}></button>
         </div>
         <div className="place-card__description">
-          <h3 className="place-card__name">{props.card.name}</h3>
+          <h3 className="place-card__name">{card.name}</h3>
           <div className="place-card__like-container">
-            <button className="place-card__like-icon"></button>
-            <p className="place-card__like-count">{props.card.likes.length}</p>
+            <button className={cardLikeButtonClassName}></button>
+            <p className="place-card__like-count">{card.likes.length}</p>
           </div>
         </div>
       </div>
